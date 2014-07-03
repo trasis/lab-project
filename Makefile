0c7f20530d59a20cb98e0e2b03f1710254bce846
doubi <sss@qq.com>
2014-07-03 15:59:29 +0800

    asd

--- Makefile
+++ Makefile
@@ -1,12 +1,17 @@
-All: Date.o Meeting.o User.o Storage.o
-	
-Date.o: Date.h Date.cpp
-	g++ -std=c++11 -c -g Date.cpp 
-Meeting.o: Meeting.h Meeting.cpp
-	g++ -std=c++11 -c -g Meeting.cpp
-User.o: User.cpp User.h
-	g++ -std=c++11 -c -g User.cpp
-Storage.o: Storage.h Storage.cpp
-	g++ -std=c++11 -c -g Storage.cpp
+CC    = g++
+FLAG  = -std=c++11
+SRC   = Date.o User.o Meeting.o Storage.o AgendaService.o
+Test : Test.cpp Date User Meeting Storage AgendaService
+	$(CC) $(FLAG) -o Test Test.cpp $(SRC)
+Date : Date.cpp
+	$(CC) $(FLAG) -c Date.cpp
+User : User.cpp Date
+	$(CC) $(FLAG) -c User.cpp
+Meeting : Meeting.cpp User Date
+	$(CC) $(FLAG) -c Meeting.cpp
+Storage : Storage.cpp User Meeting Date
+	$(CC) $(FLAG) -c Storage.cpp
+AgendaService : AgendaService.cpp User Meeting Date Storage
+	$(CC) $(FLAG) -c AgendaService.cpp
 clean:
-	rm *.o
+	rm *.o Test agenda.data
---
+++ test.cpp
@@ -0,0 +1,35 @@
+#include "Meeting.h"
+#include "Date.h"
+#include "Storage.h"
+#include "User.h"
+#include "AgendaService.h"
+#include <iostream>
+#include <string>
+#include <functional>
+
+using namespace std;
+
+int main() {
+  AgendaService agenda;
+  agenda.startAgenda();
+  if (agenda.userRegister("William", "17881243", "williamjwking@gmail.com", "8130770")) cout << "Regist sucessfully!" << endl;
+  string userName, password;
+  cout << "name:" << endl;
+  cin >> userName;
+  cout << "password:" << endl;
+  cin >> password;
+  if (agenda.userLogIn(userName, password)) cout << "Login sucessfully!" << endl;
+  if (agenda.deleteUser(userName, password)) cout << "Logout successfully!" << endl;
+  if (agenda.createMeeting("William", "Love", "Smartmoon", "2014-11-02/08:40", "2014-11-02/23:00")) cout << "Create successfully!" << endl;
+  else cout << "??" << endl;
+  if (agenda.createMeeting("William", "Love1", "Smartmoon", "2014-11-02/08:40", "2014-11-02/23:00")) cout << "Create successfully!" << endl;
+  else cout << "Have no time" << endl;
+  if (agenda.createMeeting("William", "Love2", "Smartmoon", "2014-11-03/08:40", "2014-11-02/23:00")) cout << "Create successfully!" << endl;
+  else cout << "Time < 0" << endl;
+  if (agenda.createMeeting("William", "Love", "Smartmoon", "2014-11-03/08:40", "2014-11-03/23:00")) cout << "Create successfully!" << endl;
+  else cout << "Repeat name" << endl;
+  if (agenda.createMeeting("William", "Love", "Smartmoon", "2014-11-99/08:40", "2014-11-02/23:00")) cout << "Create successfully!" << endl;
+  else cout << "Invalid Time" << endl;
+  agenda.quitAgenda();
+  return 0;
+}
